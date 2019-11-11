73201b624b0fd50b2dd0b4a6e67df184edab74b6
Hitarth Choubisa <hitarthc64@gmail.com>
2019-11-10 19:59:31 -0500

    Create interactive_plot.py

---
+++ interactive_plot.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.random.rand(15)
+y = np.random.rand(15)
+names = np.array(list("ABCDEFGHIJKLMNO"))
+x_label = 'y_label'
+y_label = 'x_label'
+
+
+def return_splot(x,y,names,x_label,y_label):
+
+    fig,ax = plt.subplots()
+    sc = plt.scatter(x,y,s=2)
+    
+    annot = ax.annotate("", xy=(0,0), xytext=(20,20),textcoords="offset points",
+                        bbox=dict(boxstyle="round", fc="w"),
+                        arrowprops=dict(arrowstyle="->"))
+    annot.set_visible(False)
+    
+    def update_annot(ind):
+    
+        pos = sc.get_offsets()[ind["ind"][0]]
+        annot.xy = pos
+        text = " ".join([names[n] for n in ind["ind"]])
+        annot.set_text(text)
+        annot.get_bbox_patch().set_facecolor(cmap(norm(c[ind["ind"][0]])))
+        annot.get_bbox_patch().set_alpha(0.4)
+    
+    
+    def hover(event):
+        vis = annot.get_visible()
+        if event.inaxes == ax:
+            cont, ind = sc.contains(event)
+            if cont:
+                update_annot(ind)
+                annot.set_visible(True)
+                fig.canvas.draw_idle()
+            else:
+                if vis:
+                    annot.set_visible(False)
+                    fig.canvas.draw_idle()
+    
+    fig.canvas.mpl_connect("motion_notify_event", hover)
+    
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.show()
